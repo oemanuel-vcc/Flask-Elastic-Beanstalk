57084e0810ef55a815faedf487053b0f15a56e26
Otto Emanuelsson <otto.emanuelsson@volvocars.com>
2021-07-13 10:02:39 +0000

    Adapting to Cloud9

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ application = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return "Continuous Delivery Demo"
+    return "Otto Demo"
 
 
 @application.route("/echo/<name>")
